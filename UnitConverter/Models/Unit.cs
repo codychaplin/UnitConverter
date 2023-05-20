a0194d595384f518ca8da23e9c8288503e597033
Cody Chaplin <codybchaplin@gmail.com>
2023-05-20 17:01:11 -0400

    Add project files.

---
+++ UnitConverter/Models/Unit.cs
@@ -0,0 +1,8 @@
+﻿namespace UnitConverter.Models;
+
+public class Unit
+{
+    public string Name { get; set; }
+    public string Abbreviation { get; set; }
+    public decimal ToBase { get; set; }
+}
---
+++ UnitConverter/Views/MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿namespace UnitConverter.Views;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    /// <summary>
+    /// Adds a number to the result.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    void BtnNum_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    /// <summary>
+    /// Swaps result to positive/negative.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    void BtnPlusMinus_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    /// <summary>
+    /// Clears fields.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    void BtnClear_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    /// <summary>
+    /// Deletes last number in result.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    void BtnBack_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    /// <summary>
+    /// Shifts focus to top Entry.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    void BtnUp_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    /// <summary>
+    /// Shifts focus to bottom Entry.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    void BtnDown_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
